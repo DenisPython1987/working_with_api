fd9e6e52a6329428f2bc2156ffd002c2889539f9
DenisPython1987 <denisandervivan2@gmail.com>
2024-06-04 21:59:21 -0300

    Alterações.

--- python_repos.py
+++ python_repos.py
@@ -4,3 +4,12 @@ import requests
 url = "https://api.github.com/search/repositories"
 url += "?q=language:python+sort:stars+stars:>10000"
 
+headers = {"Accept":"Application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+#Converte the response object to a dictionary.
+response_dict = r.json()
+
+#Process results.
+print(response_dict.keys())
